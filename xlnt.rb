29bfda2971725b8ea94c0778c95fcab283b7e9d8
Thomas Fussell <thomas.fussell@gmail.com>
2015-12-26 14:23:45 -0500

    try using binaries again

--- xlnt.rb
+++ xlnt.rb
@@ -1,14 +1,11 @@
 class Xlnt < Formula
   desc "user-friendly xlsx library for C++14"
   homepage "https://github.com/tfussell/xlnt"
-  url "https://github.com/tfussell/xlnt/archive/v0.9.0.zip"
+  url "https://github.com/tfussell/xlnt/releases/download/v0.9.0/xlnt-v0.9.0-osx.zip"
   version "0.9.0"
-  sha256 "f926269bcc7044e2378ced8bd55d8a5429e2c9759661db5510a72eff38ee4cf8"
-
-  depends_on "cmake" => :build
+  sha256 "9311d5d31691ba4aa7f2f0ce3475b4115c5e772b33d0ce11253fc14d5fd53673"
 
   def install
-    system "python", "configure", "--prefix=#{prefix}"
-    system "make", "-C", "build", "install"
+    system "cp", "-R", "./", "#{prefix}"
   end
 end
