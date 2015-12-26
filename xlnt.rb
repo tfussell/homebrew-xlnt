3fc548f88418c912a3cd2a4f522eb67095b41e2c
Thomas Fussell <thomas.fussell@gmail.com>
2015-12-26 14:06:29 -0500

    use source instead of compiled binaries

--- xlnt.rb
+++ xlnt.rb
@@ -1,7 +1,7 @@
 class Xlnt < Formula
   desc "user-friendly xlsx library for C++14"
   homepage "https://github.com/tfussell/xlnt"
-  url "https://github.com/tfussell/xlnt/releases/download/v0.9.0/xlnt-v0.9.0-osx.zip"
+  url "https://github.com/tfussell/xlnt/archive/v0.9.0.zip"
   version "0.9.0"
   sha256 "9311d5d31691ba4aa7f2f0ce3475b4115c5e772b33d0ce11253fc14d5fd53673"
 
